dfd019bd6f5bafd5ae3ef35a1433c1b7fc2c4906
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-04-18 21:28:11 +0900

    fix(frontend): fix argument name

--- www/commands/timer.ts
+++ www/commands/timer.ts
@@ -4,6 +4,12 @@ import { invoke } from "@tauri-apps/api";
  * @param duration - seconds e.g 10 * 60
  * @returns return type of callback
  */
-export async function update_info_interval(durationSec: number): Promise<void> {
-  await invoke("update_info_interval", { durationSec });
+export async function update_info_interval(
+  durationSecs: number
+): Promise<void> {
+  try {
+    await invoke("update_info_interval", { durationSecs });
+  } catch (error) {
+    console.error(error);
+  }
 }
